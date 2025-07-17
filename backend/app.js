5cad761a51401e23b4986292d304c2f2d8cc3b52
Bhawani Shankar Maru <Bhawanishankarmaru2005@gmail.com>
2025-07-17 14:54:06 +0530

    second

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 import fs from 'node:fs/promises';
-
 import bodyParser from 'body-parser';
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 const app = express();
-const port  = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
